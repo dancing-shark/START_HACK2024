0ff216ef1d4d96e9aa04e99e23f92a699c71b20b
Pascal Weissleder <pascal.weissleder.bs@gmail.com>
2024-03-21 20:26:55 +0100

    local working message- no git

---
+++ ios_app/nipa_assistant/nipa_assistant_app/nipa_assistant_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nipa_assistant_app
+//
+//  Created by Pascal Weißleder on 21.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_app/nipa_assistant/nipa_assistant_app/nipa_assistant_appUITests/nipa_assistant_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  nipa_assistant_appUITestsLaunchTests.swift
+//  nipa_assistant_appUITests
+//
+//  Created by Pascal Weißleder on 21.03.24.
+//
+
+import XCTest
+
+final class nipa_assistant_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
