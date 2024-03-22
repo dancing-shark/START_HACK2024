0f73b6c6db932050fb6a455da177f29b9652143f
nicolascampbell <nicodsm7@gmail.com>
2024-03-22 04:37:11 +0100

    improved database construction

--- database_preprocessing/test.py
+++ database_preprocessing/test.py
@@ -1,25 +1,54 @@
 import os
-from langchain_community import embeddings
-from langchain_community import vectorstores
+import bs4
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain import hub
-from langchain_groq import ChatGroq
 from langchain_community.embeddings import CohereEmbeddings
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from pathlib import Path
 from langchain_community.document_loaders import BSHTMLLoader
+import re
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_core.documents.base import Document
 
-os.environ["COHERE_API_KEY"] = "2eidazDqcsW3aRIDoIZKcsroe92z5Wx97L53cZk5"
+os.environ["COHERE_API_KEY"] = "zE6fWh7WodD48szFXjx2rX8RPEpAIqNamhjFOBL9"
 database_path = './chroma_db_full'
-def format_docs(documents):
-    return "\n\n".join(doc.page_content for doc in documents)
+
+def normalize_whitespace_and_breaklines(text):
+    # Collapse multiple newline characters into a single newline
+    text = re.sub(r'\n+', '\n', text)
+    # Replace sequences of whitespace characters (excluding newlines) with a single space
+    text = re.sub(r'[ \t]+', ' ', text)
+    # Trim leading and trailing whitespace
+    text = text.strip()
+    text = text.replace('Ã¤', 'ä').replace('Ã¶', 'ö').replace('Ã¼', 'ü').replace('ÃŸ', 'ß').replace('Ã„', 'Ä').replace('Ã–', 'Ö').replace('Ãœ', 'Ü')
+    return text
 
 def find_html_files(directory):
     root_dir = Path(directory)
     html_files = list(root_dir.rglob("*.html"))
     return html_files
+
+def create_document(page_content, source, title):
+    metadata = {
+        'source': source,
+        'title': title
+    }
+    return Document(page_content=page_content, metadata=metadata)
+
+def extract_text_from_html(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    soup = bs4.BeautifulSoup(content, 'html.parser')
+    title_element = soup.find('title')
+    title_text = title_element.text.strip() if title_element else 'No Title'
+    content_element = soup.find('div', id='content')
+     # Remove any script and style elements
+
+    if content_element:
+        for script in soup(["script", "style", "nav","button","form"]):
+            script.extract()
+        purified_text = normalize_whitespace_and_breaklines(content_element.text.strip()) 
+        return create_document(purified_text, str(file_path), title_text)
 
 def add_documents_to_vectorstore_in_batches( documents, embeddings_model, text_splitter, batch_size=96):
     # Split the documents according to the text splitter logic
@@ -38,7 +67,7 @@ def add_documents_to_vectorstore_in_batches( documents, embeddings_model, text_s
     vectorstore.persist() 
     return vectorstore
 
-root_directory = "data/bildung-sport"  
+root_directory = "data"  
 
 # This gets all html files paths recursevely
 html_files = find_html_files(root_directory)
@@ -47,33 +76,13 @@ html_files = html_files
 documents = []
 for file_path in html_files:
     # Extract the text content of the html file
-    loader = BSHTMLLoader(str(file_path))
-    # Loads content into a document obj
-    loaded_docs = loader.load()
-    documents.extend(loaded_docs)
+    extracted_text = extract_text_from_html(str(file_path)) 
+    if extracted_text:
+        loaded_doc = extracted_text
+        # Loads content into a document obj
+        documents.append(loaded_doc)
 
 embeddings_model = CohereEmbeddings(model="embed-multilingual-v3.0")
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
-splits = text_splitter.split_documents(documents)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=100)
 vectorstore = add_documents_to_vectorstore_in_batches(documents, embeddings_model, text_splitter)
-
-# The vectorstore is saved on the repo. To access to it you can do this:
-# db3 = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
-
-#--------------------FOR TESTING PURPOSES---------------------
-# Retrieve and generate using the relevant snippets of the blog.
-# retriever = vectorstore.as_retriever()
-# llm = ChatGroq(temperature=0, groq_api_key="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",model_name="mixtral-8x7b-32768")
-# prompt = hub.pull("rlm/rag-prompt")
-#
-#
-# rag_chain = (
-#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-#     | prompt
-#     | llm
-#     | StrOutputParser()
-# )
-#
-# x = rag_chain.invoke("Wer führt im Auftrag der Ostschweizer Regierungskonferenz?")
-# print(x)
